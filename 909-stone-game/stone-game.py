43504c4462b221859620198625525a3e7b4242f2
vedavyas9866971957-alt <vedavyas9866971957@gmail.com>
2026-08-02 20:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (75.06%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,12 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        @cache
-        def best(left,right):
-            if left==right:
-                return piles[left]
-
-            #choosing left
-            leftchoose=piles[left]-best(left+1,right)
-            rightchoose=piles[right]-best(left,right-1)
-            return max(leftchoose,rightchoose)
-        return best(0,len(piles)-1)>0
+        return True
