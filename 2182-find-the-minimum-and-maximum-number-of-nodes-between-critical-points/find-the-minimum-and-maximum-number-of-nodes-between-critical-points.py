1c5838c896c1400db53fc4bf2e70ef8c8abbbc06
vedavyas9866971957-alt <vedavyas9866971957@gmail.com>
2026-08-31 12:11:10 +0530

    Time: 80 ms (59.45%) | Memory: 63.1 MB (57.18%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,39 +1,38 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        if  head.next.next==None:
-            return [-1,-1] 
-        start=0
-        prev=head
-        curr=head.next
-        nex=head.next.next
-        mindis=99999
-        maxdis=-1
-        ind=0
-        crits=[-1,-1]
-        while(nex):
-            if prev.val<curr.val>nex.val or prev.val>curr.val<nex.val:
-                if crits[0]==-1:
-                    crits[0]=ind
+
+        if head is None or head.next is None or head.next.next is None:
+            return [-1, -1]
+
+        prev = head
+        curr = head.next
+        nex = curr.next
+
+        first = -1
+        last = -1
+        min_dist = float('inf')
+
+        index = 1
+
+        while nex:
+            if (curr.val > prev.val and curr.val > nex.val) or \
+               (curr.val < prev.val and curr.val < nex.val):
+
+                if first == -1:
+                    first = index
                 else:
-                    crits[1]=ind
-                if crits[1]==-1:
-                    start=ind
-                if crits[1]!=-1 and crits[1]-crits[0]<mindis:
-                    mindis=crits[1]-crits[0]
-                if crits[1]!=-1:
-                    crits[0]=crits[1] 
-            ind+=1
-            prev=curr
-            curr=nex
-            nex=nex.next
-        if crits[1]==-1:
-            return [-1,-1]
-        maxdis=crits[-1]-start
-       
-        
-        return [mindis,maxdis]
+                    min_dist = min(min_dist, index - last)
+
+                last = index
+
+            prev = curr
+            curr = nex
+            nex = nex.next
+            index += 1
+
+        if first == -1 or first == last:
+            return [-1, -1]
+
+        max_dist = last - first
+
+        return [min_dist, max_dist]
