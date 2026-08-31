5a050b3e896d53784ac3d4d198da41f88ad43a9c
vedavyas9866971957-alt <vedavyas9866971957@gmail.com>
2026-08-31 12:00:53 +0530

    Time: 83 ms (52.90%) | Memory: 63.5 MB (7.81%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -7,24 +7,33 @@ class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
         if  head.next.next==None:
             return [-1,-1] 
-        
+        start=0
         prev=head
         curr=head.next
         nex=head.next.next
         mindis=99999
         maxdis=-1
         ind=0
-        crits=[]
+        crits=[-1,-1]
         while(nex):
             if prev.val<curr.val>nex.val or prev.val>curr.val<nex.val:
-                crits.append(ind)
-                if len(crits)>=2 and crits[-1]-crits[-2]<mindis:
-                    mindis=crits[-1]-crits[-2]
+                if crits[0]==-1:
+                    crits[0]=ind
+                else:
+                    crits[1]=ind
+                if crits[1]==-1:
+                    start=ind
+                if crits[1]!=-1 and crits[1]-crits[0]<mindis:
+                    mindis=crits[1]-crits[0]
+                if crits[1]!=-1:
+                    crits[0]=crits[1] 
             ind+=1
             prev=curr
             curr=nex
             nex=nex.next
-        if len(crits)<2:
+        if crits[1]==-1:
             return [-1,-1]
-        maxdis=crits[-1]-crits[0]
+        maxdis=crits[-1]-start
+        print(start,crits[-1])
+        
         return [mindis,maxdis]
