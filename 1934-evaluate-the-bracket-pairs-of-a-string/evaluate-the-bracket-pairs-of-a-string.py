2de14a906bf899df591d5d0aff999876dcf3115d
vedavyas9866971957-alt <vedavyas9866971957@gmail.com>
2026-09-26 11:25:53 +0530

    Time: 48 ms (63.86%) | Memory: 51.4 MB (61.68%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,24 +1,30 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        hashknowledge=dict(knowledge)
+        knowledge = dict(knowledge)
 
-        res=[]
-        i=0
-        while(i<len(s)):
-            char=s[i]
-            if char=="(":
-                key=""
-                i+=1
-                while(s[i]!=")"):
-                    key+=s[i]
-                    i+=1
-                res.append(hashknowledge.get(key,"?"))
-                i+=1
+        res = []
+        i = 0
+
+        while i < len(s):
+
+            if s[i] == "(":
+                i += 1
+                start = i
+
+                while s[i] != ")":
+                    i += 1
+
+                key = s[start:i]
+                res.append(knowledge.get(key, "?"))
+
+                i += 1
+
             else:
-                st=[]
-                while(i<len(s) and s[i]!="("):
-                    st.append(s[i])
-                    i+=1
-                res.append("".join(st))
-        return  "".join(res)
-            
+                start = i
+
+                while i < len(s) and s[i] != "(":
+                    i += 1
+
+                res.append(s[start:i])
+
+        return "".join(res)
